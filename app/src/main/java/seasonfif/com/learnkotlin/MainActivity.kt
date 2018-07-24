073a28c01ecb373e50518f10108129a2b3ca77e6
zhangqiang215603 <zhangqiang215603@sogou-inc.com>
2018-07-24 12:08:52 +0800

    init project

---
+++ app/src/main/java/seasonfif/com/learnkotlin/MainActivity.kt
@@ -0,0 +1,12 @@
+package seasonfif.com.learnkotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
